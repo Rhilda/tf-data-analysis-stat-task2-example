5d3f50f5b67bd46961d0812df11d41ab5474445c
Rhilda <ildariismagilov@mail.ru>
2023-04-04 21:04:46 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,8 +7,7 @@ from scipy.stats import norm
 chat_id = 773978697 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
+    time = 71
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    n = len(x)
+    return (-min(-x) - 1/2)*2/time**2, (-np.log(alpha)/n - min(-x) - 1/2)*2/time**2
